bf6b9607490541e39baab2406aabc7ef8bd9700a
RolandoPranataAR <rolandopranata@gmail.com>
2023-02-16 13:23:39 +0700

    chore: add react-router-dom configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+import { Text } from '@chakra-ui/react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Navbar } from './components'
 
-export const App = () => <Navbar />
+export const App = () => (
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Text>Activities</Text>} />
+      <Route path="/todo" element={<Text>Todo</Text>} />
+    </Routes>
+  </BrowserRouter>
+)
