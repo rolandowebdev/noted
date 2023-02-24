c1815b0e21270a29c36466b3b8da0cc2ecdb5139
RolandoPranataAR <rolandopranata@gmail.com>
2023-02-24 21:03:33 +0700

    feat: add react-router-dom and axios into manualChunks

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          react: ['react', 'react-dom'],
+          react: ['react', 'react-dom', 'react-router-dom', 'axios'],
         },
       },
     },
